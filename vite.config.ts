f0798ffcb65e414d65d0b30a7a28e349363ad2f5
Alexandre Diano <alexandrediano.souza@outlook.com>
2023-08-04 23:42:09 -0300

    Atualizando projeto com novas pastas e configurações

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'path';
+import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: Number(process.env.VITE_PORT),
-  },
-  resolve: {
-    alias: [
-      { find: './runtimeConfig', replacement: './runtimeConfig.browser' },
-      { find: '@', replacement: path.resolve(__dirname, '/src') },
-    ],
-  },
-});
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: Number(process.env.VITE_PORT),
+    },
+    resolve: {
+      alias: [
+        { find: './runtimeConfig', replacement: './runtimeConfig.browser' },
+        { find: '@', replacement: path.resolve(__dirname, '/src') },
+        { find: '_', replacement: path.resolve(__dirname, '/test') },
+      ],
+    },
+  });
+};
